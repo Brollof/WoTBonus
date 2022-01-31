1db115856c523debb33edf96cc843bfa67b35160
Brollof <thefish3r@gmail.com>
2022-01-31 19:05:04 +0100

    win forms window removed

--- WoTBonus/App.cs
+++ WoTBonus/App.cs
@@ -8,7 +8,7 @@ using HtmlAgilityPack;
 
 namespace WoTBonus
 {
-    public partial class Form1 : Form
+    public partial class WotBonusAppContext : ApplicationContext
     {
 #if DEBUG
         [DllImport("kernel32.dll", SetLastError = true)]
@@ -22,17 +22,33 @@ namespace WoTBonus
 
         private readonly int SCRAP_TIMER_PERIOD_MS = 5000;
         private readonly string RYKOSZET_URL = "https://rykoszet.info/category/wot/";
+        private readonly NotifyIcon trayIcon = new NotifyIcon();
+        private readonly Timer timerScrap = new Timer();
         private List<string> prevTitles = null;
 
-        public Form1()
+        public WotBonusAppContext()
         {
-            InitializeComponent();
             Init();
+        }
+
+        private void Init()
+        {
+#if DEBUG
+            AllocConsole();
+#endif
+            ContextMenu menu = new ContextMenu();
+            menu.MenuItems.Add("Exit", ContextMenuExit);
+            trayIcon.ContextMenu = menu;
+            trayIcon.Icon = Properties.Resources.AppIcon;
+            trayIcon.Visible = true;
+
+            timerScrap.Tick += TimerScrap_Tick;
+            timerScrap.Interval = SCRAP_TIMER_PERIOD_MS;
+            timerScrap.Start();
         }
 
         private List<string> GetTitles()
         {
-            
             HtmlWeb web = new HtmlWeb();
             HtmlAgilityPack.HtmlDocument doc = web.Load(RYKOSZET_URL);
             HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//h2[@class='post-title entry-title']");
@@ -42,22 +58,6 @@ namespace WoTBonus
                 return nodes.Select(n => n.InnerText.Trim()).ToList();
             }
             return new List<string>();
-        }
-
-        private void Init()
-        {
-#if DEBUG
-            AllocConsole();
-#endif
-            ContextMenu menu = new ContextMenu();
-            //menu.MenuItems.Add("Temperature chart", ShowTemperatureChart);
-            menu.MenuItems.Add("Exit", ContextMenuExit);
-            notifyIcon1.ContextMenu = menu;
-
-            timerScrap.Interval = SCRAP_TIMER_PERIOD_MS;
-            timerScrap.Start();
-
-            CenterToScreen();
         }
 
         private void ContextMenuExit(object sender, EventArgs e)
@@ -70,18 +70,13 @@ namespace WoTBonus
 #if DEBUG
             FreeConsole();
 #endif
-            notifyIcon1.Visible = false;
-            notifyIcon1.Dispose();
+            trayIcon.Visible = false;
+            trayIcon.Dispose();
             Application.Exit();
             Environment.Exit(0);
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            OnExit();
-        }
-
-        private void timerScrap_Tick(object sender, EventArgs e)
+        private void TimerScrap_Tick(object sender, EventArgs e)
         {
             var newTitles = GetTitles();
             if (prevTitles == null || prevTitles[0] != newTitles[0])
@@ -89,7 +84,7 @@ namespace WoTBonus
                 if (newTitles.Any(title => title.ToLower().Contains("bonus")) == true)
                 {
                     Console.WriteLine("notify");
-                    notifyIcon1.ShowBalloonTip(5000, "WoT Bonus Code", "There is a new bonus code", ToolTipIcon.Info);
+                    trayIcon.ShowBalloonTip(5000, "WoT Bonus Code", "There is a new bonus code", ToolTipIcon.Info);
                 }
             }
             prevTitles = newTitles;
